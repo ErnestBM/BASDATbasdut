c8944a43915e3b44f38a420213c25b26aa17bef1
Ernest Benedictus Manurung <ernestbenedictusmanurung@beem.local>
2023-05-22 16:35:28 +0700

    First Commit

---
+++ CRUPeminjamanStadium/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrupeminjamanstadiumConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CRUPeminjamanStadium'
---
+++ RManagePertandingan/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RmanagepertandinganConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RManagePertandingan'
